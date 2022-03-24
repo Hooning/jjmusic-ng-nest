9e10faf65ba67d6e6db75b4beeefac0b23e9c7cb
David Cho <david.hoontime@gmail.com>
2022-03-24 22:33:05 +0100

    feat: set different style on active menu

--- libs/shared/ui/sidenav-list/src/lib/sidenav-list.component.ts
+++ libs/shared/ui/sidenav-list/src/lib/sidenav-list.component.ts
@@ -1,13 +1,27 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'shared-ui-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss'],
 })
-export class SidenavListComponent {
+export class SidenavListComponent implements OnInit {
   @Output()
   closeSidenav = new EventEmitter<void>();
+
+  currentRoute$ = new Observable<string>();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentRoute$ = this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd),
+      tap((event: any) => console.log(event.url)),
+      map((event: any) => event['url'])
+    );
+  }
 
   onClose(): void {
     this.closeSidenav.emit();
