28d0149bf4163d55c91e032e7f1f4637dd355af0
David Cho <david.hoontime@gmail.com>
2022-03-28 22:44:16 +0200

    chore: add routeModule to toolbar

--- libs/shared/ui/toolbar/src/lib/shared-ui-toolbar.module.ts
+++ libs/shared/ui/toolbar/src/lib/shared-ui-toolbar.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar.component';
 import { MaterialModule } from '@jjmusic-ng-nest/shared/utils/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   declarations: [ToolbarComponent],
   exports: [ToolbarComponent],
 })
